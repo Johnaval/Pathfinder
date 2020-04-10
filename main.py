73aabc246bd0a7dc95dfd9d712851d85204761a2
Johnaval <joaov@poli.ufrj.br>
2020-04-10 15:38:11 -0300

    Implemented GUI

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from tkinter import *
+import time
+
 class GUI:
     def __init__(self):
-        self.width = 1000
-        self.height = 1000
+        self.width = 500
+        self.height = 500
         self.rect_side = 20
         self.points = []
 
@@ -41,7 +43,18 @@ class GUI:
                 y1 = increment + j * self.rect_side
                 x2 = x1 + self.rect_side
                 y2 = y1 + self.rect_side
-                color = 'white' if self.game[i][j] == 0 else 'black'
+                if self.game[i][j] == 0:
+                    color = 'white'
+                elif self.game[i][j] == 1:
+                    color = 'black'
+                elif self.game[i][j] == 2:
+                    color = 'pink'
+                elif self.game[i][j] == 3:
+                    color = 'red'
+                elif self.game[i][j] == 4:
+                    color = 'magenta'
+                elif self.game[i][j] == 5:
+                    color = 'blue'
                 self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, tag = (i,j))
 
     def change_rect_color(self, event, value, color):
@@ -71,7 +84,54 @@ class GUI:
     def mouse_release(self, event):
         self.mouse_clicked = 3 if self.mouse_clicked == 2 else False
 
+    def find_visited(self):
+        neighbors = [[-1,0], [0,-1], [0,1], [1,0]]
+        temp_list = []
+        for i in range(len(self.unvisited_nodes)):
+            for j in range(len(self.unvisited_nodes[0])):
+                if self.unvisited_nodes[i][j][0] in (1,2) and [i,j] not in self.visited_nodes:
+                    self.visited_nodes.append([i,j])
+                    for neighbor in neighbors:
+                        if i+neighbor[0] >= 0 and i+neighbor[0] < len(self.unvisited_nodes[0]) and j+neighbor[1] >= 0 and j+neighbor[1] < len(self.unvisited_nodes):
+                            if self.unvisited_nodes[i+neighbor[0]][j+neighbor[1]][0] == 0:
+                                temp_list.append([i+neighbor[0],j+neighbor[1]])
+                                self.unvisited_nodes[i+neighbor[0]][j+neighbor[1]][1] = self.unvisited_nodes[i][j][1] + 1 if self.unvisited_nodes[i][j][1] + 1 < self.unvisited_nodes[i+neighbor[0]][j+neighbor[1]][1] else self.unvisited_nodes[i+neighbor[0]][j+neighbor[1]][1]
+                            elif self.unvisited_nodes[i+neighbor[0]][j+neighbor[1]][0] == 3:
+                                self.done = True
+        
+        for node in temp_list:
+            self.unvisited_nodes[node[0]][node[1]][0] = 1
+            self.game[node[0]][node[1]] = 3
+            if self.check_var.get() == 1:
+                time.sleep(0.01)
+                self.draw_game()
+                self.canvas.update()
+
+    def find_path(self, old_i, old_j, i, j):
+        pass 
+
     def Run(self):
-        pass
+        self.unvisited_nodes = [[[0,9999] for j in range(self.width//self.rect_side)] for i in range(self.height//self.rect_side)]
+
+        for i in range(self.height//self.rect_side):
+            for j in range(self.width//self.rect_side):
+                if self.game[i][j] == 1:
+                    self.unvisited_nodes[i][j][0] = 4
+        
+        self.unvisited_nodes[self.points[0][0]][self.points[0][1]][0] = 2
+        self.unvisited_nodes[self.points[0][0]][self.points[0][1]][1] = 0
+        self.unvisited_nodes[self.points[1][0]][self.points[1][1]][0] = 3
+
+        self.visited_nodes = []
+
+        self.done = False
+     
+        while self.done == False:
+            self.find_visited()
+
+        #self.find_path(-1,-1,self.points[0][0], self.points[0][1])
+
+        self.draw_game()
+        self.canvas.update()
 
 gui = GUI()
